8ff217089848685b738a985c0eb269ad92dc813d
hausthy <hausthy@163.com>
2021-08-09 22:23:48 +0800

    polly

--- HttpClientFactoryPolly/Startup.cs
+++ HttpClientFactoryPolly/Startup.cs
@@ -1,22 +1,21 @@
-using HostingStartupDemo.Core;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace HostingStartupDemo
+namespace HttpClientFactoryPolly
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
-            Console.WriteLine("Startup ");
-
             Configuration = configuration;
         }
 
@@ -25,20 +24,12 @@ namespace HostingStartupDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Console.WriteLine("Startup.ConfigureServices ");
-
             services.AddControllers();
-
-            services.AddHealthChecks();
-
-            services.AddHostedService<BackHostService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            Console.WriteLine("Startup.Configure ");
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,25 +38,6 @@ namespace HostingStartupDemo
             app.UseRouting();
 
             app.UseAuthorization();
-
-            app.Use(async (content, next) =>
-           {
-               await next();
-           });
-
-            //app.Run(async (context) =>
-            //{
-            //    if (context.Request.Path == "/health")
-            //    {
-            //        await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes("ok"));
-            //    }
-            //});
-
-            app.UseHealthChecks("/health", new HealthCheckOptions
-            {
-                AllowCachingResponses = true,
-                ResponseWriter = async (context, healthReport) => await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes("ok"))
-            });
 
             app.UseEndpoints(endpoints =>
             {
