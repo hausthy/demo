4b72df4bf594aaf1e36c2ce2bcdce4d9eeb6472d
hausthy <hausthy@163.com>
2021-08-14 21:16:02 +0800

    add

--- HttpClientFactoryPolly/Startup.cs
+++ HttpClientFactoryPolly/Startup.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Polly;
-using Polly.Timeout;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
